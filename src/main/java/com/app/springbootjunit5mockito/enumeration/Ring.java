80d4a1420f67d0fa00bf5291319bc8558469a6aa
shadman.shihab <shadman.shihab@bracits.com>
2024-03-27 01:07:56 +0600

    custom annotation && required models and enums added

---
+++ src/main/java/com/app/springbootjunit5mockito/enumeration/Ring.java
@@ -0,0 +1,8 @@
+package com.app.springbootjunit5mockito.enumeration;
+
+import com.app.springbootjunit5mockito.annotations.Magical;
+
+@Magical
+public enum Ring {
+    oneRing, vilya, nenya, narya, dwarfRing, manRing;
+}
